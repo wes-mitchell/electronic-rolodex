021bc4fa9abc7008bf47040db6e5d5802dceeb41
wes-mitchell <wmdrums@gmail.com>
2022-03-09 13:48:00 -0600

    adds search function to text input field

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -27,4 +27,39 @@ export const businessList = () => {
   agentArr.forEach(element => {
     agentTarget.innerHTML += Agents(element)
   } )
+
+  // Place an article element in your HTML with the class below
+const companySearchResultArticle = document.querySelector(".foundCompanies")
+
+document
+    .querySelector("#companySearch")
+    .addEventListener("keypress", keyPressEvent => {
+        if (keyPressEvent.charCode === 13) {
+            /*
+                When user presses enter, find the matching business.
+                You can use the `.includes()` method strings to
+                see if a smaller string is part of a larger string.
+
+                Example:
+                    business.companyName.includes(keyPressEvent.target.value)
+            */
+            const foundBusiness = businessArr.find(element => element.companyName.includes(keyPressEvent.target.value)) // implement .find() method here
+
+            companySearchResultArticle.innerHTML = `
+                <h2>
+                ${foundBusiness.companyName}
+                </h2>
+                <section>
+                ${foundBusiness.addressFullStreet}
+
+                </section>
+                <section>
+                ${foundBusiness.addressCity},
+                ${foundBusiness.addressStateCode}
+                ${foundBusiness.addressZipCode}
+                </section>
+            `;
+        }
+    });
 }
+
